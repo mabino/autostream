a124a0fb9c2fc927a521f9fcdf040aa405d4b55e
Michael Bino <mabino@gmail.com>
2025-04-20 08:58:37 -0400

    Initial Commit

---
+++ Streamosphere/StreamosphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamosphereApp.swift
+//  Streamosphere
+//
+//  Created by Michael Bino on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamosphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamosphereTests/StreamosphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamosphereTests.swift
+//  StreamosphereTests
+//
+//  Created by Michael Bino on 4/20/25.
+//
+
+import Testing
+@testable import Streamosphere
+
+struct StreamosphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
